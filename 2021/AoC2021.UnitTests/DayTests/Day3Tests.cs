2621456ae1f6873cb3bf862c099c2d6698add99e
Cees Kaas <34474075+CeesKaas@users.noreply.github.com>
2021-12-03 11:39:35 +0100

    Added day3 of 2021

---
+++ 2021/AoC2021.UnitTests/DayTests/Day3Tests.cs
@@ -0,0 +1,46 @@
+﻿using AoC2021.Days;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace AoC2021.UnitTests.DayTests;
+
+internal class Day3Tests
+{
+    [Test]
+    public async Task TestDay3Part1()
+    {
+        var inputMock = PrepareInput();
+        var objUnderTest = new Day3(inputMock.Object);
+        Assert.That(await objUnderTest.Part1(), Is.EqualTo(198));
+    }
+
+    [Test]
+    public async Task TestDay3Part2()
+    {
+        var inputMock = PrepareInput();
+        var objUnderTest = new Day3(inputMock.Object);
+        Assert.That(await objUnderTest.Part2(), Is.EqualTo(0));
+    }
+
+    private static Mock<InputFetcher> PrepareInput()
+    {
+        var inputMock = new Mock<InputFetcher> { CallBase = true };
+        inputMock.Setup(m => m.FetchInputAsString(3)).Returns(Task.FromResult(@"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"));
+        return inputMock;
+    }
+}
---
+++ 2021/AoC2021/Days/Day3.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Linq;
+using Utilities;
+
+namespace AoC2021.Days;
+
+public class Day3
+{
+    private readonly IInputFetcher _inputFetcher;
+    public Day3(IInputFetcher? inputFetcher = null)
+    {
+        _inputFetcher = inputFetcher ?? new InputFetcher();
+    }
+
+    internal async Task Start()
+    {
+        Console.WriteLine($"Day 3 part 1 answer: {await Part1()}");
+        Console.WriteLine($"Day 3 part 2 answer: {await Part2()}");
+    }
+
+    public async Task<int> Part1()
+    {
+        var numbers = (await _inputFetcher.GetTransformedSplitInputForDay(3, s =>
+        {
+            return new BitArray(s.Select(c => c == '1').Reverse().ToArray());
+        })).ToArray();
+        var gamma = new BitArray(new bool[numbers[0].Length]);
+
+        for (var i = 0; i < numbers[0].Length; i++)
+        {
+            var oneCount = numbers.Count(n => n[i]);
+            if (oneCount > numbers.Length / 2)
+            {
+                gamma[i] = true;
+            }
+        }
+
+        var gammaNumericArray = new int[1];
+        gamma.CopyTo(gammaNumericArray, 0);
+        var gammaNumeric = gammaNumericArray[0];
+
+        var epsilon = (BitArray) gamma.Clone();
+        epsilon.Xor(new BitArray(numbers[0].Length, true));
+        var epsilonNumericArray = new int[1];
+        epsilon.CopyTo(epsilonNumericArray, 0);
+        var epsilonNumeric = epsilonNumericArray[0];
+        return gammaNumeric * epsilonNumeric;
+    }
+
+    public async Task<int> Part2()
+    {
+        return 0;
+    }
+}
